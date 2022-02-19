6870e0814855ba0ac2035920d1c97154410033a1
NeonStrawberry <96705137+NeonStrawberry@users.noreply.github.com>
2022-02-19 16:11:46 -0400

    Add files via upload

---
+++ nft-rgb.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <math.h>
+#include <stdbool.h>
+
+double res;
+int iter_c;
+int type;
+
+double c[2];
+double z[2];
+double tmp[2];
+double focus[] = {0, 0};
+double magnification = 1;
+char *buffer;
+int colour;
+
+void iter() {
+	if (type == 0) {
+		tmp[0] = z[0] * z[0] - z[1] * z[1] + c[0];
+		tmp[1] = 2 * z[0] * z[1] + c[1];
+		
+		z[0] = tmp[0];
+		z[1] = tmp[1];
+	} else if (type == 2) {
+		tmp[0] = z[0] * z[0] - z[1] * z[1] + c[0];
+		tmp[1] = 1.2 * z[0] * z[1] + c[1];
+		
+		z[0] = tmp[0];
+		z[1] = tmp[1];
+	} else if (type == 1) {
+		tmp[0] = fabs(z[0]) * fabs(z[0]) - fabs(z[1]) * fabs(z[1]) + c[0];
+		tmp[1] = 2 * fabs(z[0]) * fabs(z[1]) + c[1];
+		
+		z[0] = tmp[0];
+		z[1] = tmp[1];
+	}
+}
+
+double dst() {
+	return (z[0] * z[0]) + (z[1] * z[1]);
+}
+
+void render() {
+	for (int i = 0; i < res * res; i++) {
+		printf("%c", buffer[i * 2]);
+		printf("%c", buffer[i * 2 + 1]);
+		
+		if ((i % (int) res == 0) && (i != 0))
+			printf("\n");
+	}
+}
+
+int main() {
+	char signature[] = "Made by NeonStrawberry";
+	
+	printf("Neon's Fractal Tool v1.3\n\n");
+	printf("What resolution would you like? ");
+	scanf("%lf", &res);
+	printf("How many iterations would you like? ");
+	scanf("%d", &iter_c);
+	printf("How deep would you like to zoom? ");
+	scanf("%lf", &magnification);
+	printf("Where would you like to zoom? \n");
+	scanf("%lf", &focus[0]);
+	scanf("%lf", &focus[1]);
+	printf("Which fractal?\n\t[0] Mandelbrot\n\t[1] Burning Ship\n\t[2] Mandelberry\n");
+	scanf("%d", &type);
+	printf("Would you like colouring?\n\t[0] No\n\t[1] Yes\n");
+	scanf("%d", &colour);
+
+	int k = 0;
+	buffer = (char *) malloc(res * res * 2 + res);
+
+	for (int i = 0; i < res; i++) {
+		for (int j = 0; j < res; j++) {
+			z[0] = 0;
+			z[1] = 0;
+		
+			if (j < res / 2)
+				c[0] = (double) ((double) j - res / 2) / ((res / 4) * magnification) + focus[0];
+			else
+				c[0] = (double) ((double) j - (res / 2 - 1)) / ((res / 4) * magnification) + focus[0];
+			
+			if (i < res / 2)
+				c[1] = (double) ((double) i - res / 2) / ((res / 4) * magnification) + focus[1];
+			else
+				c[1] = (double) ((double) i - (res / 2 - 1)) / ((res / 4) * magnification) + focus[1];
+				
+			k = 0;
+			
+			while ((dst() <= 4) && (k < iter_c + 2)) {
+				k++;
+				iter();
+			}
+			
+			if (k == iter_c + 2)
+				buffer[(i * (int) res + j) * 2] = '#';
+			else if (colour != 0) {	
+				switch (k % 3) {
+					case 0: buffer[(i * (int) res + j) * 2] = '_'; break;
+					case 1: buffer[(i * (int) res + j) * 2] = '='; break;
+					case 2: buffer[(i * (int) res + j) * 2] = '`'; break;
+				}
+			} else {
+				buffer[(i * (int) res + j) * 2] = ' ';
+			}
+			
+			buffer[(i * (int) res + j) * 2 + 1] = buffer[(i * (int) res + j) * 2];
+		}
+	}
+	render();
+	
+	free(buffer);
+	exit(0);
+}
+//ghjfghfghjfdg
